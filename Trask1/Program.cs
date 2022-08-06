f74a2029faf794662a5c680fa141061e28caaeec
Жанна Буковцева <jbukovtseva@mail.ru>
2022-08-06 20:17:40 +0300

    Trask1

---
+++ Trask1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Пожалуйста введите 2 числа,");
+Console.WriteLine("чтобы узнать какое большее, а какое меньшее.");
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine("Число " + num1 + " большее");
+    Console.Write("Число " + num2 + " меньшее");
+}
+else if (num1 == num2)
+{
+    Console.WriteLine("Число " + num1 + " равно числу " + num2);
+}
+else
+{
+    Console.WriteLine("Число " + num2 + " большее");
+    Console.Write("Число " + num1 + " меньшее");
+}
