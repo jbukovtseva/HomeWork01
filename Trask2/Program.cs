ba1985db3c2d17e388523b3e0fec330cef9fb50a
Жанна Буковцева <jbukovtseva@mail.ru>
2022-08-06 20:33:49 +0300

    Trask2

---
+++ Trask2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите три числа, и узнайте, ");
+Console.WriteLine("какое из них имеет максимальное значение.");
+Console.Write("Введите первое число и нажмите ENTER: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число и нажмите ENTER: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число и нажмите ENTER: ");
+int num3 = int.Parse(Console.ReadLine());
+int maxNum = 0;
+if (num1 > maxNum) maxNum = num1;
+if (num2 > maxNum) maxNum = num2;
+if (num3 > maxNum) maxNum = num3;
+Console.Write("Максимальное значение имеет число " + maxNum);
+
