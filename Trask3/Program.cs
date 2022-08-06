c909cc8aaa3a0d89915075186cb934b2b6cac558
Жанна Буковцева <jbukovtseva@mail.ru>
2022-08-06 20:38:18 +0300

    Trask3

---
+++ Trask3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int remain = num % 2;
+if (remain == 0)
+{
+    Console.WriteLine("Число " + num + " четное?");
+    Console.WriteLine("ДА!");
+}
+else
+{
+    Console.WriteLine("Число " + num + " четное?");
+    Console.WriteLine("НЕТ!");
+}
